850f99368544d24eafce414ee4114119fb2231ef
sandip ahire <sandipahire34@gmail.com>
2021-09-08 13:48:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ReverseContentsOfArray
+{
+    class Reverse
+    {
+        public void reverse()
+        {
+            int n;
+            int[] x = new int[20];
+            int[] y = new int[20];
+            Console.WriteLine("Enter array length:");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter array elemetns:");
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int j = n;
+            for (int i = 0; i < n; i++)
+            {
+
+                y[j - 1] = x[i];
+                j = j - 1;
+               
+
+            }    
+
+                Console.WriteLine("Reversed array is:");
+                for (int k = 0; k < n; k++)
+                {
+                    Console.WriteLine(y[k]);
+
+                }
+            
+        }
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                Reverse rr = new Reverse();
+                rr.reverse();
+            }
+        }
+    }
+}
